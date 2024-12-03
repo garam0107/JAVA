95ab8dd723854dd0417b948a0b5fbbe85d37e6d7
garam0107 <name3937@gmail.com>
2024-12-03 19:21:40 +0900

    java Day1

---
+++ java-practice/src/operator/Operator1.java
@@ -0,0 +1,16 @@
+package operator;
+
+public class Operator1 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+        int sum = a + b ;
+        System.out.println("a+b = " + sum);
+        int diff = a -b ;
+        System.out.println("a-b = " + diff);
+        int multi = a * b ;
+        int div = a / b ;
+        int mod = a % b ;
+        System.out.println("a % b = " + mod);
+    }
+}
---
+++ java-practice/src/variable/Var5.java
@@ -0,0 +1,22 @@
+package variable;
+
+public class Var5 {
+    public static void main(String[] args) {
+//        정수
+        byte b = 127; // -128 ~ 127
+        short s = 32767; // -32,768 ~ 32,767
+        int i = 2147483647; // 약 20억
+        long l = 9124124124214L; //
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        // 실수
+        float f = 10.0f;
+        double d = 10.0;
+        System.out.println(f);
+        System.out.println(d);
+
+
+    }
+}
